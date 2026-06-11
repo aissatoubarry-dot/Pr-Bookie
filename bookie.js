26c10d8cdc1bc6dc0f5c390a0ee3e0f3d1657c24
aissatoubarry-dot <aissatoubarrysow2@gmail.com>
2026-06-11 12:29:08 +0000

    deuxième ajout

--- bookie.js
+++ bookie.js
@@ -5,66 +5,73 @@ let container3 = document.querySelector(".container3");
 let btnConnecter = document.querySelector(".btnConnecter");
 let btnCreerCompte = document.querySelector(".btnCreerCompte");
 
-btnCreerCompte.addEventListener("click", function(){
 
-    const nom = document.getElementById('inscNom').value;
-    const email = document.getElementById('inscEmail').value;
-    const tel = document.getElementById('inscTel').value;
-    const mdp = document.getElementById('inscMdp').value;
-    const confirmPassword = document.getElementById('inscConfirm').value;
+btnCreerCompte.addEventListener("click",function(){
 
-    if(!nom || !email || !tel || !mdp || !confirmPassword){
-        alert("Veuillez remplir tous les champs");
+    let nom = document.getElementById("inscNom").value;
+    let email = document.getElementById("inscEmail").value;
+    let tel = document.getElementById("inscTel").value;
+    let mdp = document.getElementById("inscMdp").value;
+    let confirm = document.getElementById("inscConfirm").value;
+
+    const utilisateur = {
+        nom: nom,
+        email: email,
+        tel: tel,
+        mdp: mdp,
+        confirm: confirm
+    }
+
+    if (nom === "" || email == "" || tel === "" || mdp === "" || confirm === "") {
+        alert("Vous devez remplir tous les champs");
         return;
     }
 
-    if(mdp !== confirmPassword){
+    if (mdp !== confirm) {
         alert("Les mots de passe ne correspondent pas");
         return;
     }
 
-    let utilisateur = {
-        nom,
-        email,
-        tel,
-        mdp
-    };
-
-    localStorage.setItem(
-        "utilisateur",
-        JSON.stringify(utilisateur)
-    );
-
-    alert("Compte créé avec succès");
-
+    localStorage.setItem(utilisateur, JSON.stringify(utilisateur));
+    alert("Compte cree avec succès!");
     container2.style.display = "none";
     container1.style.display = "flex";
     container3.style.display = "none";
+
 });
 
-document.getElementById("createAccountLink").addEventListener("click", function(){
+btnConnecter.addEventListener("click",function(){
+
+    let email = document.getElementById("loginEmail").value;
+    let mdp = document.getElementById("loginMotDePasse").value;
+
+    let utilisateur = JSON.parse(localStorage.getItem("utilisateur"));
+
+    if (email === "" || mdp === "") {
+        alert("Veuillez remplir tous les champs");
+        return;
+    }
+
+    if (email === utilisateur.email && mdp === utilisateur.mdp) {
+        container1.style.display = "none";
+        container2.style.display = "none";
+        container3.style.display = "flex";
+        alert("Connexion reussie");
+    }
+    else{
+        alert("Informations incorrect");
+        return;
+    }
+
+});
+
+let creerCompteLink = document.getElementById("creerCompteLink");
+creerCompteLink.addEventListener("click",function(){
+
+    
     container1.style.display = "none";
     container2.style.display = "flex";
     container3.style.display = "none";
-});
-
-btnConnecter.addEventListener("click", function(){
-
-    const email = document.getElementById("loginEmail").value;
-    const mdp = document.getElementById("loginMotDePasse").value;
-
-    let utilisateur = JSON.parse(
-        localStorage.getItem("utilisateur") || "null"
-    );
-
-    if(utilisateur && email === utilisateur.email && mdp === utilisateur.mdp){
-        container1.style.display = "none";
-        container3.style.display = "flex";
-        alert("Connexion réussie");
-    }
-    else{
-        alert("Email ou mot de passe incorrect");
-    }
 
 });
 
